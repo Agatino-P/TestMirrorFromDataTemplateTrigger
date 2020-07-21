f53211d1dffe9d7ec38a967d2f8befdda68e8456
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-07-21 22:34:12 +0200

    Added some borders to make all more visible

--- TestMirrorFromTemplate/MainWindow.xaml.cs
+++ TestMirrorFromTemplate/MainWindow.xaml.cs
@@ -14,22 +14,16 @@ namespace TestMirrorFromTemplate
             get { return (string)GetValue(MirrorProperty); }
             set { SetValue(MirrorProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for Mirror.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty MirrorProperty =
-            DependencyProperty.Register("Mirror", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+            DependencyProperty.Register("Mirror", typeof(string), typeof(MainWindow), new PropertyMetadata("1"));
 
-
-
-        public bool IsMirror
-        {
-            get { return (bool)GetValue(IsMirrorProperty); }
-            set { SetValue(IsMirrorProperty, value); }
-        }
-        public static readonly DependencyProperty IsMirrorProperty =
-            DependencyProperty.Register("IsMirror", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
-
-
+        //public bool IsMirror
+        //{
+        //    get { return (bool)GetValue(IsMirrorProperty); }
+        //    set { SetValue(IsMirrorProperty, value); }
+        //}
+        //public static readonly DependencyProperty IsMirrorProperty =
+        //    DependencyProperty.Register("IsMirror", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
 
         public string Messaggio
         {
