db28cabd0aa52b238e540fd78e08f6e8dba23240
agatino.work@gmail.com <agatino.work@gmail.com>
2020-07-21 18:26:47 +0200

    Add project files.

---
+++ TestMirrorFromTemplate/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace TestMirrorFromTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public string Mirror
+        {
+            get { return (string)GetValue(MirrorProperty); }
+            set { SetValue(MirrorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Mirror.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MirrorProperty =
+            DependencyProperty.Register("Mirror", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+
+
+        public bool IsMirror
+        {
+            get { return (bool)GetValue(IsMirrorProperty); }
+            set { SetValue(IsMirrorProperty, value); }
+        }
+        public static readonly DependencyProperty IsMirrorProperty =
+            DependencyProperty.Register("IsMirror", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
